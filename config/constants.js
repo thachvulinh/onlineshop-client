7e5fc3a2aa271cad9f310ead68838197009a848b
THACH VU LINH <thachvulinh@gmail.com>
2022-05-01 17:51:15 +0700

    update

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,9 @@
-var url_server="http://localhost:5000" || "https://onlineshop-server.herokuapp.com";
-var url_admin="http://localhost:3001" || "https://onlineshop-admin.herokuapp.com";
+var url_server="https://onlineshop-server.herokuapp.com";
+var url_admin="http://localhost:3001";
 var clientID_facebook="1160485638048278";
 var clientSecret_facebook="5d8fb35dc605fadf48392a4828fe5e2a";
 if(url_server!="http://localhost:5000"){
+    url_admin="https://onlineshop-admin.herokuapp.com";
     clientID_facebook="263034845920099";
     clientSecret_facebook="85078d75f5e017e89dd71e89e6f3b723";
 }
